5f79742145a5938e51ac934c6c9b4e1105efe059
dbabusci <98336934+dbabusci@users.noreply.github.com>
2023-12-03 21:36:35 -0500

    Basic start to front end

---
+++ frontend/mainpage.php
@@ -0,0 +1,33 @@
+<html>
+    <div style="height: 100vh; width: 100vw; background-color: forestgreen;">
+        <div style="height: 20%;">
+            <h1 style="text-align: center;">Bank Page</h1>
+        </div>
+        <div style="height: 20%">
+            <form>
+                <label style="font-size: 4vh;">Amount to deposit:</label>
+                <input style="font-size: 4vh; height: 50%; width: 20%;" type="text">
+            </form>
+            <!--deposit mony php function here-->
+        </div>
+        <div style="height: 20%">
+            <form>
+
+            </form>
+            <label style="font-size: 4vh;">Amount to transfer:</label>
+            <input style="font-size: 4vh; height: 50%; width: 20%;" type="text">
+            <label style="font-size: 4vh;">Target Account</label>
+            <input style="font-size: 4vh; height: 50%; width: 20%;" type="text">
+            <!--transfer money php function here-->
+        </div>
+        <div style="height: 20%">
+            <label style="font-size: 4vh;">Amount to deposit:</label>
+            <input style="font-size: 4vh; height: 50%; width: 20%;" type="text">
+            <!--withdraw money php function here-->
+        </div>
+        <div style="height: 20%">
+            <p>Your balance is:</p>
+            <!--display the balance here-->
+        </div>
+    </div>
+</html>
